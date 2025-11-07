957772e665a1814deca601891e3bd28bf4b5d3db
John Aoga <johnaoga@gmail.com>
2025-11-07 13:31:52 +0100

    fixing cmake issues

--- build.sh
+++ build.sh
@@ -2,20 +2,19 @@
 
 # Build script for CppNCorr library and provide libncorr.a in the lib/ directory
 
-set -e
+# Clean previous build
+echo "Cleaning previous build..."
+rm -rf build
+rm lib/libncorr.a
 
 # Create build directory
 echo "Creating build directory..."
 mkdir -p build
 pushd build
 
-# Clean previous build
-echo "Cleaning previous build..."
-rm -rf CMakeCache.txt CMakeFiles Makefile cmake_install.cmake ../lib/libncorr.a
-
 # Run CMake
 echo "Running CMake..."
-cmake . -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake .. -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
 # Build library
 echo "Building library..."
